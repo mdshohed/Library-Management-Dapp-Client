e376f728d6efd982e543fbc15c6c3317751ecd05
mdshohed <mdshohed170@gmail.com>
2022-12-24 21:29:32 +0600

    Library-Management-Dpp-Client

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,86 +1,190 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import { useState, useEffect } from "react";
+import { ContractAddress } from "../config";
+import {ethers} from "ethers";
 
-const Home: NextPage = () => {
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+import Library from '../abi/Library.json';
+import { NextPage } from 'next'; 
+import { EtherscanProvider } from "@ethersproject/providers";
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+declare let window: any;
+import './components/Book';
+import Book from "./components/Book";
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
+const Home:NextPage =()=>{
 
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
+  const [currentAccount, setCurrentAccount] = useState(''); 
+  const [bookName, setBookName] = useState('');
+  const [bookAuthor, setBookAuthor] = useState('');
+  const [bookYear, setBookYear] = useState('');
+  const [bookFinished, setBookFinished] = useState('no');
+  const [booksFinished, setBooksFinished] = useState([]);
+  const [booksUnFinished, setBooksUnFinished] = useState([]);
 
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
+  const connectWallet =async () => {
+    try{
+      const {ethereum} = window;
+      console.log(ethereum); 
+      if(!ethereum){
+        console.log("Metamask no detected"); 
+        return; 
+      }
+      let chainId = await ethereum.request({method:'eth_chainId'});
+      const goerliChainId = '0x5';  
+      if(chainId !== goerliChainId) {
+        alert("You are not connected to Goerli network"); 
+        return;
+      }
+      const accounts = await ethereum.request({method:'eth_requestAccounts'});
+      console.log(accounts);  
+      setCurrentAccount(accounts[0]); 
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
+    }catch(error){
+      console.log("Error connecting to Metamask:", error);
+    }
+  }
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+  const submitBook =async () => {
+    let book = {
+      'name': bookName, 
+      'year': bookYear, 
+      'author': bookAuthor,
+      'finished': bookFinished =='yes' ? true : false,
+    };
+    try {
+      const {ethereum} = window;
+      if(ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const libraryContract = new ethers.Contract(ContractAddress, Library.abi, signer);
 
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
+        let libraryTx = await libraryContract.addBook(book.name, book.year, book.author, book.finished);
+        console.log(libraryTx);
+      }else{
+        console.log("Ethereum object does not exit");
+      }
+    } catch (error) {
+      console.log("Error submitting new Book.", error);
+    }
+  }
+
+  const getBooks = async()=>{
+    try {
+      const {ethereum} = window;
+      if(ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const libraryContract = new ethers.Contract(ContractAddress, Library.abi, signer);
+
+        let booksFinished = await libraryContract.getFinishedBooks();
+        let booksUnfinished = await libraryContract.getUnFinishedBooks();
+
+        setBooksFinished(booksFinished);
+        setBooksUnFinished(booksUnfinished);
+
+      }else{
+        console.log("Ethereum object does not exit");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const clickBookFinished = async (id)=>{
+    try {
+      const {ethereum} = window;
+      if(ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const libraryContract = new ethers.Contract(ContractAddress, Library.abi, signer);
+
+        let libraryTx = await libraryContract.setFinished(id, true);
+        console.log(libraryTx);
+      }else{
+        console.log("Ethereum object does not exit");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
+  return (<>
+    <div className="flex flex-col items-center bg-[#f3f6f4] text-[#6a50aa] min-h-screen pb-20">
+      <div className="transition hover:rotate-100 hover:scale-105 transition duration-500 ease-in-out">
+      </div>
+      <h2 className="text-3xl font-bold mb-20 mt-12">Manage your Personal Library</h2>
+      {
+        currentAccount === ''? (
+          <button className="text-2xl font-bold py-3 px-12 bg-[#f1c232] rounded-lg mb-10 hover:scale-105 transition" onClick={connectWallet}>Connect Wallet</button>
+        ):(
+          <div className="">
+            <div className="text-3xl font-bold mb-20 mt-12">
+              <h4>Wallet Connected: {currentAccount}</h4>
+            </div>
+
+            <div className="text-xl font-semibold mb-20 mt-4">
+              <input className="text-xl font-bold mb-2 mt-1" type="text" placeholder="Book Name" value={bookName} onChange={(e)=>setBookName(e.target.value)} />
+              <br />
+              <input className="text-xl font-bold mb-2 mt-1" type="text" placeholder="Book Author" value={bookAuthor} onChange={(e)=>setBookAuthor(e.target.value)} />
+              <br />
+              <input className="text-xl font-bold mb-2 mt-1" type="text" placeholder="Book Year" value={bookYear} onChange={(e)=>setBookYear(e.target.value)} />
+              <br />
+              <label>
+                have you finished reading this Book? 
+                <select value={bookFinished} onChange={(e)=>setBookFinished(e.target.value)}>
+                  <option value="yes">Yes</option>
+                  <option value="no">No</option>
+                </select>
+              </label>
+              <button className="text-2xl font-bold py-3 px-12 bg-[#f1c232] rounded-lg mb-10 hover:scale-105 transition" onClick={submitBook}>Add Book</button>
+            </div>
+            <div className="flex flex-col justify-center items-center">
+              <div className="font-semibold text-lg text-center mb-4">Books List</div>
+              <button className="text-xl font-bold py-3 px-12 bg-[#f1c232] rounded-lg mb-10 hover:scale-105 transition duration-500 ease-in-out" onClick={getBooks}>Get Books</button>
+              {
+                booksUnFinished.length > 0 ? <div className="font-semibold text-lg text-center mb-4 mt-5">
+                  Books Unfinished ({booksUnFinished.length})
+                </div> : <div className=""></div>
+              }
+              <div className="flex flex-row justify-center items-center">
+                {
+                  booksUnFinished.map((book)=>(
+                    <Book
+                    key={book.id}
+                    id={parseInt(book.id)}
+                    name={book.name}
+                    year={parseInt(book.year).toString()}
+                    author={book.author}
+                    finished={book.finished.toString()}
+                    clickBookFinished = {clickBookFinished}
+                    />
+                  ))}
+              </div>
+              {
+                booksFinished.length > 0 ? <div className="font-semibold text-lg text-center mb-4 mt-5">
+                  Books finished ({booksFinished.length})
+                </div> : <div className=""></div>
+              }
+                <div className="flex flex-row justify-center items-center">
+                {
+                  booksFinished.map((book)=>(
+                    <Book
+                    key={book.id}
+                    id={parseInt(book.id)}
+                    name={book.name}
+                    year={parseInt(book.year).toString()}
+                    author={book.author}
+                    finished={book.finished.toString()}
+                    clickBookFinished = {clickBookFinished}
+                    />
+                  ))
+                }
+              </div>
+            </div>
+          </div>
+        )
+      }
     </div>
-  )
+  </>)
 }
 
-export default Home
+export default Home;
